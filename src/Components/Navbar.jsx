081aefc9ee59155f35aa7526f9fd3ce887d86d4b
arasu20 <imarasu20@gmail.com>
2025-02-19 01:04:49 +0530

    create navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useRef, useState } from 'react'
 import Button from './Button';
 import { TiLocationArrow } from 'react-icons/ti';
 import { div } from 'three/tsl';
+import { useWindowScroll } from 'react-use';
+import gsap from 'gsap';
 
 const Navbar = () => {
     const navItems = ['Nexus', 'Vault', 'Prologue', 'About', 'Contact']
@@ -9,6 +11,9 @@ const Navbar = () => {
     const [isIndicatorActive, setIsIndicatorActive] = useState(false);
     const audioRef = useRef(null);
     const navContainerRef = useRef(null);
+    const [lastScrollY, setLastScrollY] = useState(0)
+    const [isNavVisible, setIsNavVisible] = useState(true);
+    const { y:currentScrollY } = useWindowScroll();
     const toggleAudioIndicator = ()=>{
         setIsAudioPlaying((prev)=>!prev);
         setIsIndicatorActive((prev)=>!prev)
@@ -21,6 +26,25 @@ const Navbar = () => {
         }
 
     },[isAudioPlaying])
+    useEffect(()=>{
+        if(currentScrollY === 0){
+            navContainerRef.current.classList.remove('floating-nav')
+        }else if(currentScrollY > lastScrollY){
+            setIsNavVisible(false);
+            navContainerRef.current.classList.add('floating-nav')
+        }else if(currentScrollY < lastScrollY){
+            setIsNavVisible(true);
+            navContainerRef.current.classList.add('floating-nav')
+        }
+        setLastScrollY(currentScrollY)
+    },[currentScrollY, lastScrollY])
+    useEffect(()=>{
+        gsap.to(navContainerRef.current, {
+            y: isNavVisible ? 0 : -100,
+            opacity: isNavVisible ? 1 : 0,
+            duration: 0.2,
+        })
+    },[isNavVisible])
 return (
     <div ref={navContainerRef} className='fixed inset-x-0 top-4 z-50 h-16 border-none transition-all duration-700 sm:inset-x-6'>
         <header className='absolute top-1/2 w-full -translate-y-1/2'>
